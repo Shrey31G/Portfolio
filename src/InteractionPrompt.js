c3a4005cbc7508d5e9644d465af03a5b0e82e300
S31G <gangwarshrey.31@gmail.com>
2025-04-18 23:49:59 +0530

    Added interactionprompt

---
+++ src/InteractionPrompt.js
@@ -0,0 +1,84 @@
+import { GameObject } from "./GameObject";
+import { Vector2 } from "./vector2";
+
+export class InteractionPrompt extends GameObject {
+  constructor() {
+    super({
+      position: new Vector2(2, -30),
+    });
+    this.opacity = 0;
+    this.targetOpacity = 1;
+    this.fadeSpeed = 0.1;
+  }
+
+  step(delta) {
+    if (this.opacity < this.targetOpacity) {
+      this.opacity = Math.min(
+        this.opacity + this.fadeSpeed,
+        this.targetOpacity
+      );
+    } else if (this.opacity > this.targetOpacity) {
+      this.opacity = Math.max(
+        this.opacity - this.fadeSpeed,
+        this.targetOpacity
+      );
+    }
+  }
+
+  drawImage(ctx, x, y) {
+    if (this.opacity <= 0) return;
+    ctx.save();
+    ctx.globalAlpha = this.opacity;
+
+    //draw a speech bubble
+
+    ctx.fillStyle = "rgba(255,255,255,0.9)";
+    ctx.strokeStyle = "#000";
+    ctx.lineWidth = 1;
+
+    const width = 100;
+    const height = 24;
+    const radius = 5;
+
+    // Rounded rectangle
+    ctx.beginPath();
+    ctx.moveTo(x + radius, y);
+    ctx.lineTo(x + width - radius, y);
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+    ctx.lineTo(x + width, y + height - radius);
+    ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+    ctx.lineTo(x + radius, y + height);
+    ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+    ctx.lineTo(x, y + radius);
+    ctx.quadraticCurveTo(x, y, x + radius, y);
+    ctx.closePath();
+
+    ctx.fill();
+    ctx.stroke();
+
+    // Add a little triangle pointing down
+    ctx.beginPath();
+    ctx.moveTo(x + width / 2 - 5, y + height);
+    ctx.lineTo(x + width / 2, y + height + 5);
+    ctx.lineTo(x + width / 2 + 5, y + height);
+    ctx.fill();
+    ctx.stroke();
+
+    // Text
+    ctx.fillStyle = "#000";
+    ctx.font = "10px Arial";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText("Press Enter", x + width / 2, y + height / 2);
+
+    ctx.restore();
+  }
+
+  show() {
+    this.targetOpacity = 1;
+  }
+
+  hide () {
+    this.targetOpacity = 0;
+  }
+}
