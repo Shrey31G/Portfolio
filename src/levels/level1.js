25ee0e67d26a5821f46101744fbdc11633afc228
S31G <gangwarshrey.31@gmail.com>
2025-04-24 04:18:51 +0530

    added walls in game

--- src/levels/level1.js
+++ src/levels/level1.js
@@ -1,16 +1,32 @@
 
 export const walls = new Set();
 
-walls.add(`64,68`); //tree
+// lone tree right
+walls.add(`224,32`)
 
-//squares
+walls.add(`64,48`); //tree left
+
+//squares 4 
 walls.add(`64,64`); 
 walls.add(`64,80`); 
 walls.add(`80,64`); 
 walls.add(`80,80`); 
+
+// 2 squares
+walls.add(`128,48`);
+walls.add(`144,48`)
 
 //water
 walls.add(`112,80`); 
 walls.add(`128,80`); 
 walls.add(`144,80`); 
 walls.add(`160,80`);
+
+// rocks
+walls.add(`192,96`);
+walls.add(`208,96`);
+walls.add(`224,96`);
+
+//tree & house
+walls.add(`208,64`)
+walls.add(`224,64`);
